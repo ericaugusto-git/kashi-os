c0d424879f35f69bc225795734cc55ee1922e5f1
Augusto <eric72001@hotmail.com>
2024-04-25 19:41:40 -0300

    Style: change on taskbar items position and responsiveness redo

--- src/Taskbar/Taskbar.tsx
+++ src/Taskbar/Taskbar.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useState } from 'react';
 import { useTheme } from '../contexts/ThemeContext';
 import styles from './Taskbar.module.scss';
 import Actions from './components/Actions/Actions';
@@ -9,17 +10,32 @@ import Skills from './components/Skills/Skills';
 import Start from './components/Start/Start';
 
 function Taskbar(){
-    const [theme] = useTheme()
+    const [theme] = useTheme();
+    const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
+    useLayoutEffect(() => {
+      function updateSize() {
+          console.log(window.innerWidth)
+        setSize([window.innerWidth, window.innerHeight]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
     return (
     <footer className={styles.menu + " " + styles[theme]}>
         <Start/>
-        <div className={styles.actions_skills}>
         <div className={styles.skills}>
-            <Skills />
-        </div>
-        <Actions/>
+        <Skills />
         </div>
         <Intro/>
+        <div className={styles.actions}>
+        <Actions />
+        </div>
+        {size[0] <= 1200 && 
+        <div className={styles.skills_actions}>
+                    <Actions />
+                    <Skills />
+        </div>}
         {/* <div className={styles.contact}>
             <Contact/>
         </div> */}
