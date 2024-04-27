d117df3ab7498fee4d2d286b340b409d7f2e248d
Augusto <eric72001@hotmail.com>
2024-04-27 16:18:24 -0300

    Feat: resume pdf window, preload of lockscreen

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,16 +1,15 @@
-import { t } from "i18next";
 import { useTranslation } from "react-i18next";
 import { Document, Page } from "react-pdf";
 import styles from "./Resume.module.scss";
 export default function Resume() {
   const { i18n } = useTranslation();
-  const triggerSave = () => {
-    const link = document.createElement('a');
-    link.href = file;
-    link.download = `Eric Augusto ${t('resume')}`; 
-    link.target = '_blank';
-    link.click();
-  }
+//   const triggerSave = () => {
+//     const link = document.createElement('a');
+//     link.href = file;
+//     link.download = `Eric Augusto ${t('resume')}`; 
+//     link.target = '_blank';
+//     link.click();
+//   }
   const file =
     i18n.resolvedLanguage == "pt-BR"
       ? "https://pub-23b2bdccea9b4dd0aa82eeba1d9c6805.r2.dev/curriculo%2FEric%20Augusto%20curr%C3%ADculo.pdf"
