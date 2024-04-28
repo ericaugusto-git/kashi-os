eb84e6da07e8a16a35f54bebd7e355874c87b6ba
Augusto <eric72001@hotmail.com>
2024-04-28 11:05:38 -0300

    Refactor: removing unused variables

--- src/StartMenu/components/Playlist/Playlist.tsx
+++ src/StartMenu/components/Playlist/Playlist.tsx
@@ -1,71 +1,70 @@
-import { useRef, useState } from 'react';
-import styles from './Playlist.module.scss'
-import pause from '../../../assets/playlist/pause.svg'
-import play from '../../../assets/playlist/play.svg'
-import samba from '../../../assets/samba.png'
-import mute from '../../../assets/playlist/mute.svg'
-import speaker from '../../../assets/playlist/speaker.svg'
-import yesterdays from '../../../assets/Yesterdays.mp3'
+import styles from './Playlist.module.scss';
+// import pause from '../../../assets/playlist/pause.svg'
+// import play from '../../../assets/playlist/play.svg'
+// import samba from '../../../assets/samba.png'
+// import mute from '../../../assets/playlist/mute.svg'
+// import speaker from '../../../assets/playlist/speaker.svg'
+// import yesterdays from '../../../assets/Yesterdays.mp3'
 
-type Music = {
-    nome: string;
-    compositor: string;
-    duration: string;
-  };
+// type Music = {
+//     nome: string;
+//     compositor: string;
+//     duration: string;
+//   };
 
 function Playlist(){
-    const audioRef = useRef<HTMLAudioElement>(null); // Type assertion
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [isMuted, setIsMuted] = useState(false);
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState(0);
+    // const audioRef = useRef<HTMLAudioElement>(null); // Type assertion
+    // const [isPlaying, setIsPlaying] = useState(false);
+    // const [isMuted, setIsMuted] = useState(false);
+    // const [currentTime, setCurrentTime] = useState(0);
+    // const [duration, setDuration] = useState(0);
 
-    const togglePlay = () => {
-    if (audioRef.current) {
-        if (isPlaying) {
-          audioRef?.current.pause();
-        } else {
-          audioRef?.current.play();
-        }
-    }
-        setIsPlaying(!isPlaying);
-      };
-      const toggleMute = () => {
-        if (audioRef.current) {
-          audioRef.current.muted = !isMuted;
-          setIsMuted(!isMuted);
-        }
-      };
+    // const togglePlay = () => {
+    // if (audioRef.current) {
+    //     if (isPlaying) {
+    //       audioRef?.current.pause();
+    //     } else {
+    //       audioRef?.current.play();
+    //     }
+    // }
+    //     setIsPlaying(!isPlaying);
+    //   };
+    //   const toggleMute = () => {
+    //     if (audioRef.current) {
+    //       audioRef.current.muted = !isMuted;
+    //       setIsMuted(!isMuted);
+    //     }
+    //   };
 
-      const previousMusic = () => {
-        if (audioRef.current) {
-          audioRef.current.currentTime = 0;
-        }
-      };
+    //   const previousMusic = () => {
+    //     if (audioRef.current) {
+    //       audioRef.current.currentTime = 0;
+    //     }
+    //   };
       
-      const handleTimeUpdate = () => {
-        if (audioRef.current) {
-          setCurrentTime(audioRef.current.currentTime);
-          setDuration(audioRef.current.duration);
-        }
-      };
+    //   const handleTimeUpdate = () => {
+    //     if (audioRef.current) {
+    //       setCurrentTime(audioRef.current.currentTime);
+    //       setDuration(audioRef.current.duration);
+    //     }
+    //   };
 
-      const handleSeek = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if (audioRef.current) {
-          const seekTime = (e.nativeEvent.offsetX / e.currentTarget.clientWidth) * duration;
-          audioRef.current.currentTime = seekTime;
-        }
-      };
+    //   const handleSeek = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    //     if (audioRef.current) {
+    //       const seekTime = (e.nativeEvent.offsetX / e.currentTarget.clientWidth) * duration;
+    //       audioRef.current.currentTime = seekTime;
+    //     }
+    //   };
     
-    const musics: Music[] = [{nome: "Samba Triste", compositor: "Baden Powel", duration: "2:38"}, 
-    {nome: "Yesterdays", compositor: "Luíz Bonfá", duration: "3:28"},
-    {nome: "Chaconne, Partita No. 2 BWV 1004", compositor: "Johann Sebastian Bach | Hilary Hahn", duration: "17:49"}];
+    // const musics: Music[] = [{nome: "Samba Triste", compositor: "Baden Powel", duration: "2:38"}, 
+    // {nome: "Yesterdays", compositor: "Luíz Bonfá", duration: "3:28"},
+    // {nome: "Chaconne, Partita No. 2 BWV 1004", compositor: "Johann Sebastian Bach | Hilary Hahn", duration: "17:49"}];
 
-    const [selected, setSelected] = useState(musics[0]);
+    // const [selected, setSelected] = useState(musics[0]);
 
-    const selectMusic = (music: Music) => {
-        setSelected(music)
-    }
+    // const selectMusic = (music: Music) => {
+    //     setSelected(music)
+    // }
 
     return <div className={styles.playlistContainer}>
     <div className={styles.cables + ' backgroundImage'} ></div>
