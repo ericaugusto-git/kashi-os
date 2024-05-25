f8565dfd0b1f4b700efa811d4b717f462d49f0a2
Augusto <eric72001@hotmail.com>
2024-05-25 18:16:25 -0300

    feat: start menu gifs through gihpy API and initial EmulatorJS

--- src/StartMenu/StartMenu.tsx
+++ src/StartMenu/StartMenu.tsx
@@ -1,24 +1,48 @@
-import { motion } from "framer-motion";
-import { useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { StartSetterContext } from "../App";
 import powerOff from "../assets/startMenu/power_off.svg";
 import sleep from "../assets/startMenu/sleep.svg";
-import cuteGif from "../assets/startMenu/start_menu.gif";
 import { WindowType } from "../constants/window";
 import { windowsTemplates } from "../constants/windowsTemplate";
-import {  usePcStatus } from "../contexts/PcStatusContext";
-import { useTheme } from "../contexts/ThemeContext";
+import { usePcStatus } from "../contexts/PcStatusContext";
+import { Themes, useTheme } from "../contexts/ThemeContext";
 import { useWindowContext } from "../contexts/WindowContext";
 import useOpenWindow from "../hooks/useOpenWindow";
 import styles from "./StartMenu.module.scss";
-
+const gifsId: { [key in Themes]: string } = {
+  dark: 'OMFfLpauGoT4c',
+  light: 'N5B19awm2YvwMwf8JE',
+  coffe: 'k8kITi9SAwe9JWbUaH'
+};
 function StartMenu() {
   const [startMenuOpen, setStartMenuOpen] = useContext(StartSetterContext);
   const osApps = windowsTemplates.filter(a=> a.appType == 'os' && !a.hideInStartMenu);
   const projects = windowsTemplates.filter(a=> a.appType == 'project' && !a.hideInStartMenu);
   const {t} = useTranslation();
-  const [theme] = useTheme()
+  const [theme] = useTheme();
+  const [gifUrl, setGifUrl] = useState('');
+  const [gifId, setGifId] = useState(gifsId[theme]); // Replace with the specific GIF ID
+
+  useEffect(() => {
+    const fetchGif = async () => {
+      try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=${import.meta.env.VITE_REACT_GIPHY_API_KEY}`);
+        const data = await response.json();
+        console.log(data)
+        setGifUrl(data.data.images.original.url);
+      } catch (error) {
+        console.error('Error fetching the GIF:', error);
+      }
+    };
+
+    fetchGif();
+  }, [gifId]);
+
+  useEffect(() => {
+    setGifId(gifsId[theme])
+  }, [theme]);
   // const windowsTemplates: WindowsTemplatesType = {
   //   ["playlist"]: {
   //     conteudo: <Playlist />,
@@ -67,70 +91,85 @@ function StartMenu() {
   // }
 
   return (
-    <motion.div
-      layout
-      style={{
-        height: !startMenuOpen ? "0" : "315px",
-        width: !startMenuOpen ? "0" : "335px",
-        left: "10px",
-        position: "fixed",
-        bottom: "61px",
-        zIndex:10
-      }}
-      //   initial={{ scaleY: 0, transformOrigin: 'bottom center' }}
-      //   animate={{ scaleY: 1 }}
-      //   exit={{scaleY: 0, transformOrigin: 'top center'}}
-      //   transition={{ duration: 0.1 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-    >
-      <div className={styles.start_menu + " " + styles[theme]}>
-        <div className={styles.col_1}>
-          <div
-            className={styles.cute_gif}
-            style={{ backgroundImage: `url("${cuteGif}")` }}
-          ></div>
-          <menu>
-            <ul>
-              <li>
-                <button className={styles.sleep} onClick={handleSleep}>
-                  <img src={sleep}></img>
-                  <span>{t('sleep')}</span>
-                </button>
-              </li>
-              <li>
-                <button className={styles.power_off} onClick={handlePowerOff}>
-                  <img src={powerOff}></img>
-                  <span>{t('shut')}</span>
-                </button>
-              </li>
-            </ul>
-          </menu>
-        </div>
-        <menu className={styles.col_2}>
-          {appsSections.map((section) => (
-            <li key={section.title}>
-            <span className={styles.title}>{t(section.title)}</span>
-            <ul>
-              {section.apps.map((app) => (
-                <li key={app.app}>
-                  <button onClick={() => section.handleOnClick(app)}>
-                    {/* <img src={app.icon}></img> */}
-              {app.svgMask?.startMenu ? (
-            <div style={{ maskImage: `url("${app.icon}")` }} className={"svgMask " + styles.icon}></div>
-          ) : <img src={app.icon}></img>}
-                    <span>{t(app.app)}</span>
+    <AnimatePresence>
+{   startMenuOpen &&   <motion.div
+      key="startmenu"
+        layout
+        // style={{ overflow: "hidden",           left: "10px",
+        // position: "fixed",
+        // bottom: "61px",
+        // zIndex:10 }}
+        // initial={{ scaleX: 0, opacity: 0, }}
+        // animate={{ scaleX: 1, opacity:1, transformOrigin: "center left" }}
+        // transition={{ duration: 0.2 }}
+        // exit={{ scaleY: "0", opacity: 0, transformOrigin: "bottom center" }}
+        initial={{ width: 0, height: 0, opacity: 0 }}
+        animate={{ width: "335px", height: "315px", opacity: 1 }}
+        exit={{ overflow: 'hidden', width: 0, height: 0, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        style={{
+          height: "315px",
+          width:  "335px",
+          left: "10px",
+          position: "fixed",
+          bottom: "61px",
+          zIndex:10
+        }}
+          // initial={{ scaleY: 0, transformOrigin: 'bottom center' }}
+          // animate={{ scaleY: 1 }}
+          // exit={{opacity: 0, transformOrigin: 'bottomm center'}}
+          // transition={{ duration: 0.3 }}
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1 }}
+        // exit={{ opacity: 0 }}
+        // transition={{ duration: 0.5 }}
+      >
+        <div className={styles.start_menu + " " + styles[theme]}>
+          <div className={styles.col_1}>
+            <div
+              className={styles.cute_gif}
+              style={{ backgroundImage: `url("${gifUrl}")` }}
+            ></div>
+            <menu>
+              <ul>
+                <li>
+                  <button className={styles.sleep} onClick={handleSleep}>
+                    <img src={sleep}></img>
+                    <span>{t('sleep')}</span>
                   </button>
                 </li>
-              ))}
-            </ul>
-            </li>
-          ))}
-        </menu>
-      </div>
-    </motion.div>
+                <li>
+                  <button className={styles.power_off} onClick={handlePowerOff}>
+                    <img src={powerOff}></img>
+                    <span>{t('shut')}</span>
+                  </button>
+                </li>
+              </ul>
+            </menu>
+          </div>
+          <menu className={styles.col_2}>
+            {appsSections.map((section) => (
+              <li key={section.title}>
+              <span className={styles.title}>{t(section.title)}</span>
+              <ul>
+                {section.apps.map((app) => (
+                  <li key={app.app}>
+                    <button className={styles.app_button} onClick={() => section.handleOnClick(app)}>
+                      {/* <img src={app.icon}></img> */}
+                {app.svgMask?.startMenu ? (
+              <div style={{ maskImage: `url("${app.icon}")` }} className={"svgMask " + styles.icon}></div>
+            ) : <img src={app.icon}></img>}
+                      <span>{t(app.app)}</span>
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              </li>
+            ))}
+          </menu>
+        </div>
+      </motion.div>}
+    </AnimatePresence>
   );
 }
 
