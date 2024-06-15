f11dffb4209e8a7d15c315e48f40bfbf842fc724
Augusto <eric72001@hotmail.com>
2024-06-15 10:29:06 -0300

    build: :green_heart: removing unused variables

--- src/Desktop/components/Lofi/Gifs.tsx
+++ src/Desktop/components/Lofi/Gifs.tsx
@@ -3,6 +3,6 @@ import styles from './Gifs.module.scss';
 
 export default function Gifs({gifsActive, setGifId, gifsRef}: {gifsActive: boolean, setGifId:React.Dispatch<React.SetStateAction<string>>, gifsRef: React.RefObject<HTMLDivElement>}) {
     return <div ref={gifsRef}  className={`${styles.gifs} ${gifsActive && styles.active}`}>
-        {gifs.map((gif, index) => <div key={gif.id} onClick={() => setGifId(gif.id)} className={`backgroundImage ${styles.gif}`} style={{backgroundImage: `url("${gif.url}")`}}></div>)}
+        {gifs.map((gif) => <div key={gif.id} onClick={() => setGifId(gif.id)} className={`backgroundImage ${styles.gif}`} style={{backgroundImage: `url("${gif.url}")`}}></div>)}
     </div>
 }
