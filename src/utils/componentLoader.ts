882f597865307178cdfbf29e5edf20f98ed55707
Augusto <eric72001@hotmail.com>
2024-11-17 12:18:05 -0300

    testing fix dynamically imported module in prod

--- src/utils/componentLoader.ts
+++ src/utils/componentLoader.ts
@@ -2,11 +2,11 @@ import { lazy } from 'react';
 
 export const loadComponent = (componentPath: string) => {
   // Remove the @ prefix and handle the path properly
-  // const normalizedPath = componentPath.replace('@/', '../');
+  const normalizedPath = componentPath.replace('@/', '../');
   
   return lazy(() => {
     // Using dynamic import with proper path resolution
-    return import("../Folder/Folder.tsx")
+    return import(/* @vite-ignore */ normalizedPath + '.tsx')
       .then(module => ({ default: module.default }))
       .catch(error => {
         console.error(`Error loading component ${componentPath}:`, error);
